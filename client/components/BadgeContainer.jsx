4e2a9882c2fb0b6e551c01d39fb818f8def2e8ba
Amy Chen <66530059+designal46@users.noreply.github.com>
2021-06-09 11:40:06 -0400

    badge display logic

--- client/components/BadgeContainer.jsx
+++ client/components/BadgeContainer.jsx
@@ -4,10 +4,33 @@ const BadgeContainer = () => {
 
   // props needed:
   // app status
+
+  const incompleteView = () => {
+    return (
+      <div>
+        <h2>Keep Going!</h2>
+        <div>
+          <img src="assets/icon.png" />
+        </div>
+      </div>
+    )
+  }
+
+  const completeView = () => {
+    // add logic to reference image src based on the jobID
+    return (
+      <div>
+        <h2>You Did It!</h2>
+        <div>
+          <img src="assets/icon.png" />
+        </div>
+      </div>
+    )
+  }
   
   return (
-    <div style={{border: "solid 1px black"}}>
-      Badge Container
+    <div style={{display: "flex", flexDirection: "column", border: "solid 1px black"}}>
+      
     </div>
   )
 }

33101d7b36c5fb77c63dbecc4436bc25129fd222
jenessachap <jenessachap@gmail.com>
2021-06-10 09:17:13 -0700

    changes to login page

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Redirect, Link, Route } from 'react-router-dom';
+import { useState } from 'react';
+
 
 const Login = (props) => {
 
-    function handleLogin(e){
-        e.preventDefault(); 
-        props.setIsLoggedIn(true)
-    }
+    // function handleLogin(e){
+    //     e.preventDefault(); 
+    //     props.setIsLoggedIn(true)
+    // }
 
+    //onClick={(e) => {handleLogin(e)}}
+
+
+    const [redirect, setRedirect] = useState(false);
+    if (redirect === false) {
+        console.log('in the redirect')
     return (
     <div id ='login'>
-        <button id='button-login' onClick={(e) => {handleLogin(e)}}>Sign in</button>
+        <a href='/auth/google'>Sign in with Google </a>
     </div>
     )
+    setRedirect(true);
+} else if (redirect === true) {
+    return <Redirect to='/'/>;
+}
 }
 
 export default Login;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 
 //auth login
 router.get('/login', (req, res) => {
-    console.log('inside login router.get')
+    console.log('inside login router.get', req.body)
 
     //here is where we serve up our login page
     res.send('Hello?')
@@ -16,6 +16,21 @@ router.get('/google', passport.authenticate('google', {
     scope: ['profile','email']
 }))
 
+router.get('/google/redirect',passport.authenticate('google',{
+    successRedirect: 'http://localhost:3333/',
+    failureRedirect: '/auth/error',
+  })
+);
+
+
+
+// router.get('/google/redirect',passport.authenticate('google'),(req, res) => {
+//     // res.send(req.user)
+//     //sending our OAuth user data
+//     console.log('about to redirect home')
+//     res.redirect('/')
+// });
+
 //auth logout
 router.get('/logout', (req, res) => {
     //this will be handled later with passport
@@ -25,7 +40,7 @@ router.get('/logout', (req, res) => {
 
 //callback route for google to redirect to 
 
-// this will be used to check if a user is logged in so they can see their profile, or if they should be sent to the login page
+// // this will be used to check if a user is logged in so they can see their profile, or if they should be sent to the login page
 // const authCheck = (req,res,next) => {
 // if(!req.user){
 //     //if user is not logged in
@@ -34,11 +49,6 @@ router.get('/logout', (req, res) => {
 //     next()
 // }
 // }
-router.get('/google/redirect',passport.authenticate('google'),(req, res) => {
-    // res.send(req.user)
-    //sending our OAuth user data
-    console.log('about to redirect home', res)
-    res.redirect('/')
-});
+
 
 module.exports = router;
